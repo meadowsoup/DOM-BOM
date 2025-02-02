028e8d1d7de9fbdb6bb5799dd7d190b96cedf106
adelavega <136393880+meadowsoup@users.noreply.github.com>
2025-02-01 21:23:57 -0500

    sec commit

--- index.js
+++ index.js
@@ -1,51 +1,161 @@
-const gameContainer = document.getElementById("game-container");
+const app = document.getElementById("app");
 
-const messageEl = document.createElement("p");
-const attemptsEl = document.createElement("p");
+const title = document.createElement("h1")
+title.textContent = "Take a Guess!";
 
-gameContainer.appendChild(messageEl);
-gameContainer.appendChild(attemptsEl);
+const instructions = document.createElement("p")
+instructions.textContent = "Number between 1 and 100, any guesses?";
 
-let ranNum = Math.floor(Math.random() * 100) + 1;
-let attLeft = 10;
+const startButton = document.createElement("button")
+startButton.textContent = "Start!";
 
-function updateGameUI(message, color) {
-     messageEl.textContent = message
-     messageEl.style.color = color
-     attemptsEl.textContent = `Attempts Left: ${attLeft}`
-};
+const message = document.createElement("p")
 
-function playGame() {
-     if (attLeft <= 0) {
-          setTimeout(() => alert(`😭Game Over😭 ... The number was ${ranNum}.`), 100)
-          updateGameUI(`Damn bro, it was ${ranNum} 😓`, "red")
-          return
-     };
 
-     setTimeout(() => {
-          let uGuess = prompt("Enter a number between 1 and 100:")
-          if (uGuess === null) {
-               alert("🤡")
-               return
-          };
+// const ranNum = Math.floor(Math.random() * 100) + 1;
+// let attempts = 7;
 
-          uGuess = Number(uGuess);
 
-          if(isNaN(uGuess) || uGuess < 1 || uGuess > 100) {
-               alert("👹Enter a valid # between 1 and 100👹")
-               playGame()
-               return
-          };
 
-          if (uGuess === ranNum) {
-               updateGameUI("Congrats🎊! You guessed correctly👍🏽", "green")
-               setTimeout(() => alert("Winner winner, chicken dinner. The number was " + ranNum), 100)
-          } else {
-               attLeft--
-               updateGameUI(uGuess > ranNum ? "⬆️too large, try again!" : "⬇️too small, try again!", "orange")
-               playGame()
-          };
-     }, 100);
-};
 
-setTimeout(playGame, 500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const gameContainer = document.getElementById("game-container");
+
+// const messageEl = document.createElement("p");
+// const attemptsEl = document.createElement("p");
+
+// gameContainer.appendChild(messageEl);
+// gameContainer.appendChild(attemptsEl);
+
+// let ranNum = Math.floor(Math.random() * 100) + 1;
+// let attLeft = 10;
+
+// function updateGameUI(message, color) {
+//      messageEl.textContent = message
+//      messageEl.style.color = color
+//      attemptsEl.textContent = `Attempts Left: ${attLeft}`
+// };
+
+// function playGame() {
+//      if (attLeft <= 0) {
+//           setTimeout(() => alert(`😭Game Over😭 ... The number was ${ranNum}.`), 100)
+//           updateGameUI(`Damn bro, it was ${ranNum} 😓`, "red")
+//           return
+//      };
+
+//      setTimeout(() => {
+//           let uGuess = prompt("Enter a number between 1 and 100:")
+//           if (uGuess === null) {
+//                alert("🤡")
+//                return
+//           };
+
+//           uGuess = Number(uGuess);
+
+//           if(isNaN(uGuess) || uGuess < 1 || uGuess > 100) {
+//                alert("👹Enter a valid # between 1 and 100👹")
+//                playGame()
+//                return
+//           };
+
+//           if (uGuess === ranNum) {
+//                updateGameUI("Congrats🎊! You guessed correctly👍🏽", "green")
+//                setTimeout(() => alert("Winner winner, chicken dinner. The number was " + ranNum), 100)
+//           } else {
+//                attLeft--
+//                updateGameUI(uGuess > ranNum ? "⬆️too large, try again!" : "⬇️too small, try again!", "orange")
+//                playGame()
+//           };
+//      }, 100);
+// };
+
+// setTimeout(playGame, 500);
